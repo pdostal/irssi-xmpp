ca97085f0e144d9840a825b043af3c175358ad6e
cdidier <cdidier>
2007-08-23 20:41:41 +0000

    change coding style...

--- src/core/xmpp-protocol.h
+++ src/core/xmpp-protocol.h
@@ -1,4 +1,4 @@
-/* $Id: xmpp-protocol.h,v 1.2 2007/08/15 12:44:14 cdidier Exp $ */
+/* $Id: xmpp-protocol.h,v 1.3 2007/08/23 20:41:41 cdidier Exp $ */
 
 #ifndef __XMPP_PROTOCOL_H
 #define __XMPP_PROTOCOL_H
@@ -8,17 +8,20 @@
 #define XMPP_RECODE_IN 0
 #define XMPP_RECODE_OUT 1
 
-gchar  *xmpp_recode(const gchar *, const int);
+__BEGIN_DECLS
+gchar   *xmpp_recode(const gchar *, const int);
 
-gchar      *xmpp_jid_get_username(const gchar *);
-gchar      *xmpp_jid_get_ressource(const gchar *);
-gchar      *xmpp_jid_strip_ressource(const gchar *);
-gboolean    xmpp_jid_have_ressource(const gchar *);
-gboolean    xmpp_jid_have_address(const gchar *);
-gboolean    xmpp_priority_out_of_bound(const int);
+gchar	*xmpp_jid_get_username(const gchar *);
+gchar	*xmpp_jid_get_ressource(const gchar *);
+gchar	*xmpp_jid_strip_ressource(const gchar *);
+gboolean xmpp_jid_have_ressource(const gchar *);
+gboolean xmpp_jid_have_address(const gchar *);
+gboolean xmpp_priority_out_of_bound(const int);
 
-void    xmpp_send_message_chat(XMPP_SERVER_REC *, const gchar *, const gchar *);
-void    xmpp_set_presence(XMPP_SERVER_REC *, const int, const gchar *);
-void    xmpp_register_handlers(XMPP_SERVER_REC *);
+void     xmpp_send_message_chat(XMPP_SERVER_REC *, const gchar *,
+	     const gchar *);
+void     xmpp_set_presence(XMPP_SERVER_REC *, const int, const gchar *);
+void     xmpp_register_handlers(XMPP_SERVER_REC *);
+__END_DECLS
 
 #endif
