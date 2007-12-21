0462cf9aec2a18d9184514b1cc89dfb03d4e2b1f
cdidier <cdidier>
2007-12-21 17:03:35 +0000

    build infrastructure doesn't link the modules fe-common and fe-text to loudmouth anymore

--- src/rules.mk
+++ src/rules.mk
@@ -1,31 +1,31 @@
-# $Id: rules.mk,v 1.3 2007/12/13 11:55:43 cdidier Exp $
+# $Id: rules.mk,v 1.4 2007/12/21 17:03:35 cdidier Exp $
 
 include ../../config.mk
 
-OBJS = $(SRCS:.c=.o)
+OBJS = ${SRCS:.c=.o}
 
 all: ${LIB}
 
 .c.o:
 	@echo ${CC} -c $<
-	@$(CC) -c $< $(CFLAGS)
+	@${CC} -c $< ${CFLAGS}
 
 ${LIB}: $(OBJS)
 	@echo ${CC} -o lib$@.so
-	@${CC} -o lib$@.so $(OBJS) ${LDFLAGS}
+	@${CC} -o lib$@.so ${OBJS} ${LDFLAGS}
 
 clean:
 	@echo cleaning
-	@rm -f lib$(LIB).so ${OBJS}
+	@rm -f lib${LIB}.so ${OBJS}
 
 install: all
-	@echo installing the module lib$(LIB).so to ${DESTDIR}${IRSSI_LIB}/modules
+	@echo installing the module lib${LIB}.so to ${DESTDIR}${IRSSI_LIB}/modules
 	@install -d ${DESTDIR}${IRSSI_LIB}/modules
-	@install lib$(LIB).so ${DESTDIR}${IRSSI_LIB}/modules
+	@install lib${LIB}.so ${DESTDIR}${IRSSI_LIB}/modules
 
 uninstall:
-	@echo deinstalling the module lib$(LIB).so from ${DESTDIR}${IRSSI_LIB}/modules
-	@rm -f ${DESTDIR}${IRSSI_LIB}/modules/lib$(LIB).so
+	@echo deinstalling the module lib${LIB}.so from ${DESTDIR}${IRSSI_LIB}/modules
+	@rm -f ${DESTDIR}${IRSSI_LIB}/modules/lib${LIB}.so
 
 user-install:
 	@env DESTDIR= IRSSI_LIB=~/.irssi ${MAKE} install
