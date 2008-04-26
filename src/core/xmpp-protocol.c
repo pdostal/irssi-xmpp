2cc6c3bb7df9c84c118977402b12a18fb841251b
cdidier <cdidier>
2008-04-26 11:01:11 +0000

    fix bug #11577: irssi-xmpp ignores messages without a 'type' attribute (report and patch by Thomas Frauendorfer)

--- src/core/xmpp-protocol.c
+++ src/core/xmpp-protocol.c
@@ -1,5 +1,5 @@
 /*
- * $Id: xmpp-protocol.c,v 1.54 2008/04/05 20:50:45 cdidier Exp $
+ * $Id: xmpp-protocol.c,v 1.55 2008/04/26 11:01:11 cdidier Exp $
  *
  * Copyright (C) 2007 Colin DIDIER
  *
@@ -353,6 +353,13 @@ handle_message(LmMessageHandler *handler, LmConnection *connection,
 	case LM_MESSAGE_SUB_TYPE_HEADLINE:
 	case LM_MESSAGE_SUB_TYPE_NORMAL:
 	case LM_MESSAGE_SUB_TYPE_CHAT:
+	/* rfc3921.txt states in 2.1.1. Types of Message:
+	 *   if an application receives a message with no 'type' attribute or the
+	 *   application does not understand the value of the 'type' attribute
+	 *   provided, it MUST consider the message to be of type "normal"
+	 * Thus default belongs here, whre LM_MESSAGE_SUB_TYPE_NORMAL is
+	 */
+	default:
 		/* XEP-0022: Message Events */
 		child = lm_message_node_get_child(msg->node, "x");
 		if (child != NULL) {
@@ -508,9 +515,6 @@ handle_message(LmMessageHandler *handler, LmConnection *connection,
 			g_free(nick);
 		}
 
-		break;
-
-	default:
 		break;
 	}
 
