d999774cb384253d7844e5b4909b189a76d285b7
cdidier <cdidier>
2008-08-18 22:53:44 +0000

    first bits of account registration (/XMPPREGISTER) disabled because it mysteriously doesn't work yet (based on the example in loudmouth sources)

---
+++ src/core/register.h
@@ -0,0 +1,11 @@
+/* $Id: register.h,v 1.1 2008/08/18 22:53:45 cdidier Exp $ */
+
+#ifndef __REGISTER_H
+#define __REGISTER_H
+
+__BEGIN_DECLS
+void	xmpp_register(const char *, int, const gboolean, const char *,
+	    const char *);
+__END_DECLS
+
+#endif
