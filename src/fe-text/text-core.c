361502319ca8a98c53670385702753a0c012173b
cdidier <cdidier>
2008-08-19 21:54:00 +0000

    fix fe-text/ and do some cleanup

--- src/fe-text/text-core.c
+++ src/fe-text/text-core.c
@@ -1,5 +1,5 @@
 /*
- * $Id: text-xmpp.c,v 1.5 2008/03/01 17:57:21 cdidier Exp $
+ * $Id: text-core.c,v 1.1 2008/08/19 21:54:00 cdidier Exp $
  *
  * Copyright (C) 2007 Colin DIDIER
  *
@@ -20,14 +20,12 @@
 #include "module.h"
 #include "modules.h"
 
-#include "text-xmpp-composing.h"
-#include "text-xmpp-nick.h"
+#include "text-composing.h"
 
 void
 text_xmpp_init(void)
 {
-	text_xmpp_composing_init();
-	text_xmpp_nick_init();
+	text_composing_init();
 
 	module_register("xmpp", "text");
 }
@@ -35,6 +33,5 @@ text_xmpp_init(void)
 void
 text_xmpp_deinit(void)
 {
-	text_xmpp_composing_deinit();
-	text_xmpp_nick_deinit();
+	text_composing_deinit();
 }
