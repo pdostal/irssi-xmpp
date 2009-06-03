d710c21b00283eef6f64df189f6191f75bfc555c
cdidier <cdidier>
2009-06-03 15:10:11 +0000

    don't hide what is going on during the compilation

--- src/rules.mk
+++ src/rules.mk
@@ -1,4 +1,4 @@
-# $Id: rules.mk,v 1.6 2008/08/15 00:25:21 cdidier Exp $
+# $Id: rules.mk,v 1.7 2009/06/03 15:10:11 cdidier Exp $
 
 include ../../config.mk
 
@@ -7,30 +7,27 @@ OBJS = ${SRCS:.c=.o}
 all: ${LIB}
 
 .c.o:
-	@echo ${CC} -c $<
-	@${CC} -c $< -o $@ ${CFLAGS}
+	${CC} -c $< -o $@ ${CFLAGS}
 
 ${LIB}: ${OBJS}
-	@echo ${CC} -o lib$@.so
-	@${CC} -o lib$@.so ${OBJS} ${LDFLAGS}
+	${CC} -o lib$@.so ${OBJS} ${LDFLAGS}
 
 clean:
-	@echo cleaning
-	@rm -f lib${LIB}.so ${OBJS}
+	rm -f lib${LIB}.so ${OBJS}
 
 install: all
 	@echo installing the module lib${LIB}.so to ${DESTDIR}${IRSSI_LIB}/modules
-	@install -d ${DESTDIR}${IRSSI_LIB}/modules
-	@install lib${LIB}.so ${DESTDIR}${IRSSI_LIB}/modules
+	install -d ${DESTDIR}${IRSSI_LIB}/modules
+	install lib${LIB}.so ${DESTDIR}${IRSSI_LIB}/modules
 
 uninstall:
 	@echo deinstalling the module lib${LIB}.so from ${DESTDIR}${IRSSI_LIB}/modules
-	@rm -f ${DESTDIR}${IRSSI_LIB}/modules/lib${LIB}.so
+	rm -f ${DESTDIR}${IRSSI_LIB}/modules/lib${LIB}.so
 
 user-install:
-	@env DESTDIR= IRSSI_LIB=~/.irssi ${MAKE} install
+	env DESTDIR= IRSSI_LIB=~/.irssi ${MAKE} install
 
 user-uninstall:
-	@env DESTDIR= IRSSI_LIB=~/.irssi ${MAKE} uninstall
+	env DESTDIR= IRSSI_LIB=~/.irssi ${MAKE} uninstall
 
 .PHONY: clean install uninstall user-install user-uninstall
