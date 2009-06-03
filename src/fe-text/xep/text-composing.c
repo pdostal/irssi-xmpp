f8462c5701ed76cd301e43a6304ea60ec9eee980
cdidier <cdidier>
2009-06-03 16:25:35 +0000

    remove copy of statusbar.h and use the one from irssi

--- src/fe-text/xep/text-composing.c
+++ src/fe-text/xep/text-composing.c
@@ -1,5 +1,5 @@
 /*
- * $Id: text-composing.c,v 1.1 2008/08/30 17:08:02 cdidier Exp $
+ * $Id: text-composing.c,v 1.2 2009/06/03 16:25:35 cdidier Exp $
  *
  * Copyright (C) 2007 Colin DIDIER
  *
@@ -22,16 +22,14 @@
 #include "module.h"
 #include "module-formats.h"
 #include "signals.h"
+#include "statusbar-item.h"
 #include "window-items.h"
-
-/* in include/irssi/src/fe-text */
-#include "statusbar.h"
 
 #include "xmpp-servers.h"
 #include "xmpp-queries.h"
 
 static void
-item_xmpp_composing(SBAR_ITEM_REC *item, int get_size_only)
+item_xmpp_composing(struct SBAR_ITEM_REC *item, int get_size_only)
 {
 	XMPP_SERVER_REC *server;
 	XMPP_QUERY_REC *query;
@@ -49,7 +47,7 @@ item_xmpp_composing(SBAR_ITEM_REC *item, int get_size_only)
 out:
 	if (str == NULL) {
 		if (get_size_only)
-			item->min_size = item->max_size = 0;
+			statusbar_item_set_size(item, 0, 0);
 		return;
 	}
 	statusbar_item_default_handler(item, get_size_only,
